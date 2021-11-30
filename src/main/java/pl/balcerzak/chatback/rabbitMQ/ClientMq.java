68786e09dd69cb8edeed2db0bf57fd5b5d0b56a5
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-12-01 00:26:29 +0100

    - utworzenie kolejki dla usera, który jeszcze jej nie ma - zbidnowanie utworzonej kolejki z MyExchange - pobranie wiadomości ze swojej kolejki

--- src/main/java/pl/balcerzak/chatback/rabbitMQ/ClientMq.java
+++ src/main/java/pl/balcerzak/chatback/rabbitMQ/ClientMq.java
@@ -1,11 +1,18 @@
 package pl.balcerzak.chatback.rabbitMQ;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
@@ -15,13 +22,32 @@ public class ClientMq {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @GetMapping("/receiveMessage")
-    public String get() {
-        Object message = rabbitTemplate.receiveAndConvert("topic");
+    @Autowired
+    private AmqpAdmin amqpAdmin;
 
-        System.out.println(Objects.requireNonNull(message));
+    private final FanoutExchange fanoutExchange = new FanoutExchange("MyExchange");
+    private Map<String, Queue> usersQueueMap = new HashMap<>();
+
+    @GetMapping("/receiveMessage")
+    public String get(@RequestParam String user) {
+
+        if (usersQueueMap.get(user) == null) {
+            usersQueueMap.put(user, new Queue(user));
+            bindeQueue(usersQueueMap.get(user));
+        }
+        Object message = rabbitTemplate.receiveAndConvert(user);
+
+        if (message == null) {
+            return "Brak wiadomośći w kolejce BACKEND";
+        }
         return Objects.requireNonNull(message).toString();
 
+    }
+
+    public void bindeQueue(Queue queue) {
+        amqpAdmin.declareExchange(fanoutExchange);
+        amqpAdmin.declareQueue(queue);
+        amqpAdmin.declareBinding(BindingBuilder.bind(queue).to(fanoutExchange));
     }
 
     /*@RabbitListener(queues = "topic")
